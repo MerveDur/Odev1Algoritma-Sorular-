03e1e05490bee07e55a8d650f952577087df12b4
MerveDur <merve__dur@hotmail.com>
2022-07-19 19:34:46 +0300

    Q4uestion1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloukl World!");
+            int n=0,i=0,num=0;
+            Console.WriteLine("Enter a positive number for n: ");
+            n=Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+            
+            if(n>0)
+            {
+                for(i=0;i<n;i++)
+                {
+                    
+                    num =Convert.ToInt32(Console.ReadLine());
+                    numbers[i]=num;
+                }
+                Console.WriteLine("The even numbers: ");
+                for(i=0;i<n;i++)
+                {
+                    if(numbers[i]%2==0)
+                    {    
+                        Console.WriteLine(numbers[i]+" ");                    
+                    }
+                }
+                
+            }
             
         }
     }
