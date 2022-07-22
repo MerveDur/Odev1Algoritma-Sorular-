9b0ce51dc72cf1c4d53c3119d478bd18b2c1f228
MerveDur <merve__dur@hotmail.com>
2022-07-22 16:36:17 +0300

    All question added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int n=0,i=0,num=0;
+            int n=0,i=0,num=0,m=0,n2=0,letter=0;
+            Console.WriteLine("Question 1: ");
             Console.WriteLine("Enter a positive number for n: ");
             n=Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[n];
@@ -26,6 +27,60 @@ namespace HelloWorld
                     }
                 } 
             }
+            Console.WriteLine("Question 2: ");
+            Console.WriteLine("Enter a positive number for m: ");
+            Console.WriteLine("Enter a positive number for n: ");
+            m=Convert.ToInt32(Console.ReadLine());
+            n2=Convert.ToInt32(Console.ReadLine());
+            int[] numbers2 = new int[n2];
+            if(n2>0)
+            {
+                for(i=0;i<n2;i++)
+                {
+                    num =Convert.ToInt32(Console.ReadLine());
+                    numbers2[i]=num;
+                }
+                Console.WriteLine("Result: ");
+                for(i=0;i<n2;i++)
+                {
+                    if(numbers2[i]%m==0)
+                    {    
+                        Console.WriteLine(numbers2[i]+" ");                    
+                    }
+                } 
+            }
+            Console.WriteLine("Question 3: ");
+            Console.WriteLine("Enter a positive number for n: ");
+            n=Convert.ToInt32(Console.ReadLine());
+            string[] words=new string[n];
+            if(n>0)
+            {
+                for(i=0;i<n;i++)
+                {
+                    string? str=Console.ReadLine();
+                    if(str!=null)
+                    {
+                        words[i]=str;
+                    }
+                    
+                }
+                Console.WriteLine("Reverse Words");
+                for(i=n-1;i>=0;i--)
+                {
+                    Console.WriteLine(words[i]+" ");                    
+                    
+                } 
+            }
+            Console.WriteLine("Question 4: ");
+            string? sentence=Console.ReadLine();
+            var numberOfWords = sentence.Split(' ',StringSplitOptions. RemoveEmptyEntries).Length;
+            Console.WriteLine("Question 4: "+numberOfWords);
+            for(i=0;i<sentence.Length;i++)
+            {
+                if(Char.IsLetter(sentence[i])==true)
+                    letter++;                                 
+            } 
+            Console.WriteLine(letter);
         }
     }
 }
